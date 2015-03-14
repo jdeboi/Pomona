29583b958dce1aecce87d43001fda7d1f9d6d8ff
jdeboi <jdeboi@gmail.com>
2015-03-13 22:01:19 -0500

    bike ready?

--- Xylophone/Xylophone.ino
+++ Xylophone/Xylophone.ino
@@ -1,21 +1,29 @@
-//Jenna deBoisblanc, 2011
-//Code adapted from:
-//Mark Demers' code, Copywrite 2009 (http://spikenzielabs.com/SpikenzieLabs/DrumKitKit.html)
-//Georg Mill's YAAMIDrum (www.georgmill.de/)
-//Arduino on the 4051 (http://www.arduino.cc/playground/Learning/4051)
+/*
+* Arduinio Xylophone
+* March 2015
+* Pomona College
+*
+* CREDIT
+* Mark Demers Copyright 2009 http://spikenzielabs.com/SpikenzieLabs/DrumKitKit.html
+* Georg Mill's YAAMIDrum www.georgmill.de/
+* 
+* This exercise is designed to introduce multiplexers. Check out the Arduino
+* reference on the 4051 http://www.arduino.cc/playground/Learning/4051
+*
+*/
 
 #define midichannel 0;
 int speakerPin = 3;
-unsigned char PadNote[8] = {60,62,64,65,67,69,71,72};	    //array containing MIDI note values, 
-                                                            
+unsigned char PadNote[8] = {60, 62, 64, 65, 67, 69, 71, 72};	 //array containing MIDI note values,
+
 unsigned char status;
-int PadCutOff[8] = {80,110,110,110,80,110,110,110};	    //array containing "threshold" values; 
-                                                            //increase (up to 1023) in order to make piezos less sensitive  
-int MaxPlayTime[8] = {90,90,90,90,90,90,90,90};		    //time each note remains on after being hit				
-									
-boolean activePad[8] = {0,0,0,0,0,0,0,0};                   // array of flags of pad currently playing
-int PinPlayTime[8] = {0,0,0,0,0,0,0,0};                     // counter since pad started to play
-boolean VelocityFlag = false;				    // set this variable to true if you'd like the volume                                          
+int PadCutOff[8] = {80, 110, 110, 110, 80, 110, 110, 110};	 //array containing "threshold" values;
+                                                                 //increase (up to 1023) in order to make piezos less sensitive
+int MaxPlayTime[8] = {90, 90, 90, 90, 90, 90, 90, 90};		 //time each note remains on after being hit
+
+boolean activePad[8] = {0, 0, 0, 0, 0, 0, 0, 0};            // array of flags of pad currently playing
+int PinPlayTime[8] = {0, 0, 0, 0, 0, 0, 0, 0};              // counter since pad started to play
+boolean VelocityFlag = false;				    // set this variable to true if you'd like the volume
 int analogPin = A0;                                         //to correspond to force of hit
 int hitavg = 0;
 int pad = 0;
@@ -32,95 +40,83 @@ int count = 0;   //which y pin we are selecting
 int multiplex[8];
 
 //*******************************************************************************************************************
-// Setup   
+// Setup
 //*******************************************************************************************************************
 
-void setup()
-{
+void setup() {
   pinMode(2, OUTPUT);    // s0
   pinMode(3, OUTPUT);    // s1
   pinMode(4, OUTPUT);    // s2
-  Serial.begin(57600);     
+  Serial.begin(57600);
 }
 
 //*******************************************************************************************************************
-// Main Program   
+// Main Program
 //*******************************************************************************************************************
 
-void loop (){
+void loop () {
   readSensors();
   checkSensors();
 }
 
 void readSensors () {
-  for (count=0; count<=7; count++) {
-  
-    /////////// select the bit for digital out sent to multiplexer to cycle through 8 inputs /////////////////// 
-    r2 = bitRead(count,0);    	        // code that assigns values to digital pins
-    r1 = bitRead(count,1);     
-    r0 = bitRead(count,2);      
-
-    digitalWrite(2, r0);		//sets the digital output pins to high or low (from values above)
+  ///////////////////
+  // MULTIPLEXER
+  ///////////////////
+  for (count = 0; count <= 7; count++) {
+    r2 = bitRead(count, 0);    	      
+    r1 = bitRead(count, 1);
+    r0 = bitRead(count, 2);
+    digitalWrite(2, r0);		
     digitalWrite(3, r1);
     digitalWrite(4, r2);
-  
-    
-    multiplex[count] = analogRead(analogPin);	  //each element of array "multiplex" corresponds to the  
-                                                  //voltage read at a particular piezo
+    multiplex[count] = analogRead(analogPin);
 
-    /*Serial.print(count);		          //use these functions to test/print out count (effectively piezo number) 
+    /*Serial.print(count);		          //use these functions to test/print out count (effectively piezo number)
     Serial.print(" ");                            //and voltage (0-1023) of each piezo
     Serial.println(multiplex[count]);
-    */ 
+    */
   }
 }
 
-
-
-void checkSensors() {		//function to get values of each piezo; only checking a single analog input pin
-
-  for(int pin=0; pin <=7; pin++){
+void checkSensors() {		
+  for (int pin = 0; pin <= 7; pin++) {
     hitavg = multiplex[pin];			//variable hitavg equals the voltage (0-1023) of the piezo
-    pad=pin;
-    
-    if((hitavg > PadCutOff[pin])){		//if the voltage of the piezo is higher than the value of the 
-                                                //"threshold" element in array PadCutOff, then:
-      if((activePad[pad] == false)){	        //and if the pad wasn't already on or "active"  
-      
-        if(VelocityFlag == true){
-          hitavg = (hitavg / 8) -1;		//and if you want force to correspond to volume (VelocityFlag=true), 
+    pad = pin;
+    if ((hitavg > PadCutOff[pin])) {		
+      if ((activePad[pad] == false)) {	 
+        if (VelocityFlag == true) {
+          hitavg = (hitavg / 8) - 1;		
         }					//set voltage of piezo into volume (or "velocity") range of MIDI note (0-127)
-        else{
+        else {
           hitavg = 127;				//if you don't care, set velocity to max value (127)
         }
-    
-        playKey(144, PadNote[pad],hitavg);       //put together MIDI message; see - http://spikenzielabs.com/SpikenzieLabs/Serial_MIDI_files/Midi%20Message%20Ill2.png
-        PinPlayTime[pad] = 0;			//reset the pin play time
-        activePad[pad] = true;			//make the pin active (was inactive)
+        playKey(144, PadNote[pad], hitavg);      //put together MIDI message; see - http://spikenzielabs.com/SpikenzieLabs/Serial_MIDI_files/Midi%20Message%20Ill2.png
+        PinPlayTime[pad] = 0;			
+        activePad[pad] = true;			
       }
-      
-      else {					//if the pad was already active when it was hit, increment its play time
-         PinPlayTime[pad] = PinPlayTime[pad] + 1;
+      else {					
+        PinPlayTime[pad] = PinPlayTime[pad] + 1;
       }
     }
-  
-    else if((activePad[pad] == true)){ 	//the pad is active, but it is not greater than cutoff, increment play time
+    else if ((activePad[pad] == true)) { 	
       PinPlayTime[pad] = PinPlayTime[pad] + 1;
-    
-      if(PinPlayTime[pad] > MaxPlayTime[pad]){	//but if it's already been on for the amount set in the MaxPlayTime array, 
-        activePad[pad] = false;                 //turn it off		
-        playKey(128, PadNote[pad],127);  //128 is the "status" value which corresponds to an OFF state ()
+      if (PinPlayTime[pad] > MaxPlayTime[pad]) {	
+        activePad[pad] = false;               
+        playKey(128, PadNote[pad], 127); //128 is the "status" value which corresponds to an OFF state ()
       }
     }
   }
 }
 
 
-
+////////////////////////////////////////////////////////////////
+// MAKE SOUND
+////////////////////////////////////////////////////////////////
 void playKey(unsigned char MESSAGE, unsigned char PITCH, unsigned char VELOCITY) {
   // MIDI_TX(MESSAGE,PITCH,VELOCITY);
   noTone(speakerPin);
-  int duration = map(VELOCITY, 0, 127, 0, 1500); 
+  int duration = map(VELOCITY, 0, 127, 0, 1500);
   tone(speakerPin, PITCH, duration);
 }
 
